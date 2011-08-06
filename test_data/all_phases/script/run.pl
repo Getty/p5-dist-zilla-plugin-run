6aaaae6a5c3a8fa5464756af0c0e58b6203ac6f0
Randy Stauner <randy@magnificent-tears.com>
2011-08-06 10:56:32 -0700

    Add new directory of test data to test new functionality
    for testing all defined phases/plugins and format specifiers.

---
+++ test_data/all_phases/script/run.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Path::Class;
+
+#my $fh = dir($ARGV[ 0 ], 'lib')->file('AFTER_BUILD.txt')->openw();
+my $fh = file(__FILE__)->parent->file('phases.txt')->open('>>');
+print $fh join(' ', @ARGV) . "\n";
+close $fh;

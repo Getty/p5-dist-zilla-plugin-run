8f2c633d122493b7be209c1b3095778a356d964e
Randy Stauner <randy@magnificent-tears.com>
2011-09-09 20:09:49 -0700

    Binmode test file for accurate test data
    stupid windows

--- test_data/all_phases/script/run.pl
+++ test_data/all_phases/script/run.pl
@@ -7,5 +7,6 @@ use Path::Class;
 
 #my $fh = dir($ARGV[ 0 ], 'lib')->file('AFTER_BUILD.txt')->openw();
 my $fh = file(__FILE__)->parent->file('phases.txt')->open('>>');
+binmode $fh;
 print $fh join(' ', @ARGV) . "\n";
 close $fh;

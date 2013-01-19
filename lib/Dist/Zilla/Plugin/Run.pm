b5676791af5f9480aa0f89f8fbc1807d16e8b81a
Randy Stauner <randy@magnificent-tears.com>
2013-01-18 19:09:46 -0700

    Add Run::AfterMint plugin

--- lib/Dist/Zilla/Plugin/Run.pm
+++ lib/Dist/Zilla/Plugin/Run.pm
@@ -31,7 +31,7 @@ for passing as arguments to the specified commands
 
 =for :list
 * C<%a> the archive of the release (available to all C<*Release> phases)
-* C<%d> the directory in which the dist was built (not in C<BeforeBuild>)
+* C<%d> the directory in which the dist was built (or minted) (not in C<BeforeBuild>)
 * C<%n> the dist name
 * C<%p> path separator ('/' on Unix, '\\' on Win32... useful for cross-platform dist.ini files)
 * C<%v> the dist version
---
+++ lib/Dist/Zilla/Plugin/Run/AfterMint.pm
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::Run::AfterMint;
+# ABSTRACT: Execute a command after a new dist is minted
+use Moose;
+with qw(
+  Dist::Zilla::Role::AfterMint
+  Dist::Zilla::Plugin::Run::Role::Runner
+);
+
+use namespace::autoclean;
+
+sub after_mint {
+  my ($self, $param) = @_;
+  $self->call_script({
+    dir     => $param->{mint_root},
+    minting => 1,
+  });
+}
+
+=head1 SYNOPSIS
+
+  [Run::AfterMint]
+  run = some command %d
+
+=head1 DESCRIPTION
+
+This plugin executes the specified command after minting a new dist.
+
+=head1 CONVERSIONS
+
+See L<Dist::Zilla::Plugin::Run/CONVERSIONS>
+for the list of common formatting variables available to all plugins.
+
+=cut
+
+1;

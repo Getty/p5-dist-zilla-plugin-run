67c28c4ef2976a9a5f01f599d7a9e53e5b0c8c13
Karen Etheridge <ether@cpan.org>
2016-02-12 18:06:55 -0800

    add a bit more pod markup

--- lib/Dist/Zilla/Plugin/Run.pm
+++ lib/Dist/Zilla/Plugin/Run.pm
@@ -127,10 +127,10 @@ for passing as arguments to the specified commands and eval strings
 * C<%a> the archive of the release (available to all C<*Release> phases)
 * C<%d> the directory in which the distribution was built (or minted) (not in C<BeforeBuild>)
 * C<%n> the distribution name
-* C<%p> path separator ('/' on Unix, '\\' on Win32... useful for cross-platform dist.ini files)
+* C<%p> path separator ('/' on Unix, '\\' on Win32... useful for cross-platform F<dist.ini> files)
 * C<%v> the distribution version
 * C<%t> C<-TRIAL> if the release is a trial release, otherwise the empty string
-* C<%x> full path to the current perl interpreter (like $^X but from L<Config>)
+* C<%x> full path to the current perl interpreter (like C<$^X> but from L<Config>)
 
 Additionally C<%s> is retained for backward compatibility.
 Each occurrence is replaced by a different value

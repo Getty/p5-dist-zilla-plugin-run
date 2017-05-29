65b9d41f4e5cf6bf05e962b58cf9bd2ad8a000c6
Karen Etheridge <ether@cpan.org>
2017-05-29 14:33:04 -0700

    squash

--- t/lib/TestHelper.pm
+++ t/lib/TestHelper.pm
@@ -7,13 +7,13 @@ sub is_path
     goto \&Test::More::is if $^O ne 'MSWin32';
 
     my ($got, $want, $test_name) = @_;
-    local $Test::Builder::Level = $Test::Builder::Level + 1;
 
     $got =~ s{\\}{/}g;
 
     $test_name ||= 'path name matches';
     $test_name .= ' (where / and \\ are considered identical)';
 
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
     is($got, $want, $test_name);
 }
 
@@ -23,13 +23,13 @@ sub like_path
     goto \&Test::More::like if $^O ne 'MSWin32';
 
     my ($got, $want_re, $test_name) = @_;
-    local $Test::Builder::Level = $Test::Builder::Level + 1;
 
     $got =~ s{\\}{/}g;
 
     $test_name ||= 'path name matches';
     $test_name .= ' (where / and \\ are considered identical)';
 
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
     like($got, $want_re, $test_name);
 }
 
